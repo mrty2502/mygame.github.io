04ac6a049c4f7503d6d677c33df2e98a2907ece5
mrty2502 <buiducthinh2502@gmail.com>
2020-02-19 13:55:00 +0700

    Initial commit

---
+++ appmygame.js
@@ -0,0 +1,9 @@
+function yeschoice() {
+    alert("Xin hãy tự trọng em ey :)");
+
+}
+function setranpos() {
+    document.getElementById('no').style.top = Math.floor(Math.random() * 500) + "px";
+    document.getElementById('no').style.left = Math.floor(Math.random() * 400) +"px";
+
+}
